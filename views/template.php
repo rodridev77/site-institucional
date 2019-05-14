236449986d784331644f80b13d79327329c0eca4
Markus Paulo <markus@gmail.com>
2019-05-14 17:21:15 -0300

    Criando o template e o style

---
+++ views/template.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Site Institucional</title>
+        <link rel="stylesheet" href="/assets/css/template.css">
+    </head>
+    <body>
+        <div class="topo">
+
+        </div>
+
+        <div class="menu">
+
+        </div>
+
+        <div class="container">
+            <?php $this->loadViewTemplate($viewName, $viewData); ?>
+        </div>
+    </body>
+</html>
